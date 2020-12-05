9c7d97ce4fab25cc25fc90031169486de75e0d92
anshuldaksh2016 <anshuldaksh2016@gmail.com>
2020-12-05 11:44:56 +0530

    quiz questions and rendering question in quiz component

---
+++ src/Helper/Questions.js
@@ -0,0 +1,20 @@
+export const Questions = [
+    {
+        questionId: 1,
+        question: "Who was the creator of Linux",
+        optionA: "James Gosling.",
+        optionB: "Linus Torvalds",
+        optionC: "Guido van Rossum.",
+        optionD: "Bjarne Stroustrup.",
+        answer: "optionB"
+    },
+    {
+        question: "Which of this is Not a Programming Language",
+        optionA: "Python",
+        optionB: "Ruby",
+        optionC: "Golang",
+        optionD: "Unix",
+        answer: "optionD"
+    }
+
+]
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import '../App.css'
+import { Questions } from '../Helper/Questions';
+
+import { useContext } from 'react';
+import { GameStateContext } from '../Helper/Contexts';
 
 const Quiz = () => {
+    const { userName } = useContext(GameStateContext);
     return (
-        <div>
-            <h1>Quiz</h1>
+        <div className="Quiz">
+            <h1>Welcome {userName}</h1>
+
+            {
+                Questions.map(quest => {
+                    return (
+                        <>
+                            <h2>{quest.question}</h2>
+                            <p>{quest.optionA}</p>
+                            <p>{quest.optionB}</p>
+                            <p>{quest.optionC}</p>
+                            <p>{quest.optionC}</p>
+                        </>
+                    )
+                })
+            }
+
         </div>
     )
 }
 
-export default Quiz
+export default Quiz;
