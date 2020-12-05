6aae7f7f53959f4a4a2154bdc9681a173f4a5fb2
anshuldaksh2016 <anshuldaksh2016@gmail.com>
2020-12-05 10:53:01 +0530

    context api setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
 import Menu from './components/Menu'
-
+import { GameStateContext } from './Helper/Contexts';
 
 
 //[menu , play  , finished]
 const App = () => {
 
   const [gameState, setGameState] = useState('menu')
+  //make this a global state
+
 
   return (
     <div className="App">
       <h1>Quiz App</h1>
-      {
-        gameState === 'menu' && (
-          <Menu />
-        )
-      }
+      <GameStateContext.Provider value={{ gameState, setGameState }}>
+        {
+          gameState === 'menu' && (
+            <Menu />
+          )
+        }
+      </GameStateContext.Provider>
+      {/* set the state accessible to each and every component  */}
+
       {/*<Menu s={gameState} />*/}
     </div>
   );
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../App.css'
-
+import { useContext } from 'react';
+import { GameStateContext } from '../Helper/Contexts';
 
 const Menu = () => {
 
