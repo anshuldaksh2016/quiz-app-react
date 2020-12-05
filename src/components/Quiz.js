9ffa7c49b67ca0ff37904179338487300ec74c6b
anshuldaksh2016 <anshuldaksh2016@gmail.com>
2020-12-05 11:59:34 +0530

    Answering questions

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css'
 import { Questions } from '../Helper/Questions';
 
@@ -7,23 +7,35 @@ import { GameStateContext } from '../Helper/Contexts';
 
 const Quiz = () => {
     const { userName } = useContext(GameStateContext);
+    //maintain a state for questions index and another for answered 
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [optionChosen, setOptionChosen] = useState("")
+
+    function chooseOption(option) {
+        setOptionChosen(option)
+    }
+
     return (
         <div className="Quiz">
             <h1>Welcome {userName}</h1>
 
-            {
-                Questions.map(quest => {
-                    return (
-                        <>
-                            <h2>{quest.question}</h2>
-                            <p>{quest.optionA}</p>
-                            <p>{quest.optionB}</p>
-                            <p>{quest.optionC}</p>
-                            <p>{quest.optionC}</p>
-                        </>
-                    )
-                })
-            }
+            <h2>{Questions[currentQuestion].question}</h2>
+            <div className="questions">
+                <button onClick={() => { chooseOption('optionA') }}>
+                    {Questions[currentQuestion].optionA}
+                </button>
+                <button onClick={() => { chooseOption('optionB') }}>{Questions[currentQuestion].optionB}</button>
+                <button
+                    onClick={() => { chooseOption('optionC') }}>
+                    {Questions[currentQuestion].optionC}
+                </button>
+
+                <button
+                    onClick={() => { chooseOption('optionD') }}>
+                    {Questions[currentQuestion].optionD}
+                </button>
+            </div>
+            {optionChosen}
 
         </div>
     )
